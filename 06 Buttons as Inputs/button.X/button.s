f98fb4315bf211951f50a251aca388322c73c254
Binder Tronics <29162669+Devilbinder@users.noreply.github.com>
2022-07-10 01:47:55 +0200

    button

---
+++ 06 Buttons as Inputs/button.X/button.s
@@ -0,0 +1,94 @@
+; When assembly code is placed in a psect, it can be manipulated as a
+; whole by the linker and placed in memory.  
+;
+; In this example, barfunc is the program section (psect) name, 'local' means
+; that the section will not be combined with other sections even if they have
+; the same name.  class=CODE means the barfunc must go in the CODE container.
+; PIC18's should have a delta (addressible unit size) of 1 (default) since they
+; are byte addressible.  PIC10/12/16's have a delta of 2 since they are word
+; addressible.  PIC18's should have a reloc (alignment) flag of 2 for any
+; psect which contains executable code.  PIC10/12/16's can use the default
+; reloc value of 1.  Use one of the psects below for the device you use:
+
+;psect   barfunc,local,class=CODE,delta=2 ; PIC10/12/16
+
+#include <xc.inc>
+    
+; CONFIG
+    CONFIG  FOSC = HS             ; Oscillator Selection bits (HS oscillator)
+    CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled)
+    CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+    CONFIG  BOREN = OFF           ; Brown-out Reset Enable bit (BOR disabled)
+    CONFIG  LVP = OFF             ; Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+    CONFIG  CPD = OFF             ; Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+    CONFIG  WRT = OFF             ; Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+    CONFIG  CP = OFF              ; Flash Program Memory Code Protection bit (Code protection off)  
+
+    
+DELAY_A equ 0x70
+DELAY_B equ 0x71
+DELAY_C equ 0x72
+
+psect   RESET_VECT,class=CODE,delta=2 ; PIC10/12/16
+RESET_VECT:
+    goto setup
+    
+psect   INT_VECT,class=CODE,delta=2 ; PIC10/12/16
+INT_VECT:   
+    retfie
+setup:
+    bsf STATUS,5 ;select bank 1
+    CLRF TRISB
+    BSF TRISD,2
+    BSF TRISD,3
+    bcf STATUS,5 ;select bank 0
+    CLRF PORTB    
+    bcf STATUS,0 ;select bank 0
+main:
+    BTFSS PORTD,2
+    call debounce_button1
+    BTFSS PORTD,3
+    call debounce_button2
+    BTFSC PORTD,2
+    BCF PORTB,2
+    BTFSC PORTD,3
+    BCF PORTB,3
+    
+    goto main
+
+debounce_button1:
+    BSF PORTB,2
+    call delay_100ms
+    return
+    
+debounce_button2:
+    BSF PORTB,3
+    call delay_100ms
+    return
+    
+delay_100ms:
+    movlw 0xFF ;255
+    movwf DELAY_A
+delay_a:
+    call delay_nest_b
+    decfsz DELAY_A
+    goto delay_a
+    return
+delay_nest_b:
+    movlw 112
+    movwf DELAY_B
+delay_b:
+    call delay_nest_c
+    decfsz DELAY_B
+    goto delay_b
+    return
+delay_nest_c:
+    movlw 2
+    movwf DELAY_C
+delay_c:
+    decfsz DELAY_C
+    goto delay_c
+    return
+    
+    END RESET_VECT
+    
